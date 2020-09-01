1854b8b6e283a5347f7cfd143f787dbc5e3bbe0c
Frode Hansen <frode.hansen@bekk.no>
2020-09-01 09:55:28 +0200

    Update import

--- src/common/framework/LoadingPage.tsx
+++ src/common/framework/LoadingPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import LoadingSpinner from '@navikt/sif-common-core/lib/components/loading-spinner/LoadingSpinner';
-import Page from '../../navikt/sif-common-core/components/page/Page';
+import Page from '@navikt/sif-common-core/lib/components/page/Page';
 
 const LoadingPage = () => (
     <Page title={'Henter informasjon'}>
