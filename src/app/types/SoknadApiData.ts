e9a373e693730069cf36f95be3b2e0b46d92e656
Aleksei Poroshin <aleksei.poroshin@nav.no>
2020-09-01 14:22:35 +0200

    Apidata

--- src/app/types/SoknadApiData.ts
+++ src/app/types/SoknadApiData.ts
@@ -1,7 +1,39 @@
 import { Locale } from '@navikt/sif-common-core/lib/types/Locale';
+import { Arbeidssituasjon, OverforeTilType } from './SoknadFormData';
+import { ApiStringDate } from '@navikt/sif-common-core/lib/types/ApiStringDate';
+
+export interface AndreBarnApiData {
+    fnr: string;
+    ingenFnr: boolean;
+    navn: string;
+}
+
+export interface BarnApiData {
+    fornavn: string;
+    etternavn: string;
+    mellomnavn?: string;
+    aktørId: string;
+    fødselsdato: ApiStringDate;
+}
 
 export interface SoknadApiData {
     språk: Locale;
     harForståttRettigheterOgPlikter: boolean;
     harBekreftetOpplysninger: boolean;
+    andreBarn: AndreBarnApiData[];
+    harAleneomsorg: boolean;
+    harAleneomsorgFor: Array<BarnApiData | AndreBarnApiData>;
+    harUtvidetRett: boolean;
+    harUtvidetRettFor: Array<BarnApiData | AndreBarnApiData>;
+    borINorge: boolean;
+    arbeidINorge: boolean;
+    arbeidssituasjon: Arbeidssituasjon[];
+    antallDagerHarBruktEtter1Juli: number;
+    harDeltDagerMedAndreTidligere: boolean;
+    antallDagerHarDeltMedAndre: number;
+    overforeTilType: OverforeTilType;
+    fnrMottaker: string;
+    navnMottaker: string;
+    antallDagerTilOverfore: number;
+    harBekreftetMottakerOpplysninger: boolean;
 }
