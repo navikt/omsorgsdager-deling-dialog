3d210c06fe38f3cef2a3678c748535d19022d0fa
Frode Hansen <frode.hansen@bekk.no>
2020-09-04 20:23:43 +0200

    Remove unused interface

--- src/app/types/SoknadFormData.ts
+++ src/app/types/SoknadFormData.ts
@@ -49,9 +49,6 @@ export interface Barn {
     fødselsdato: Date;
 }
 
-export interface RegistrerteBarn {
-    barn: Barn[];
-}
 export interface SoknadFormData {
     [SoknadFormField.harForståttRettigheterOgPlikter]: boolean;
     [SoknadFormField.harBekreftetOpplysninger]: boolean;
