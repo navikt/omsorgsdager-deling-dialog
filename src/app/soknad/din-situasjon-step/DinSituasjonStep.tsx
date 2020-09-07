3d58463ef6271d5e24759299a209f7f9bb4aff9d
Frode Hansen <frode.hansen@bekk.no>
2020-09-07 11:39:40 +0200

    Stop user if arbeiderINorge === false

--- src/app/soknad/din-situasjon-step/DinSituasjonStep.tsx
+++ src/app/soknad/din-situasjon-step/DinSituasjonStep.tsx
@@ -10,6 +10,7 @@ import {
     validateYesOrNoIsAnswered,
 } from '@navikt/sif-common-core/lib/validation/fieldValidations';
 import { useFormikContext } from 'formik';
+import FormQuestion from '../../../common/form-question/FormQuestion';
 import { SoknadFormData, SoknadFormField } from '../../types/SoknadFormData';
 import { getArbeidssituasjonOptions } from '../shared/shared-form-elements';
 import SoknadFormComponents from '../SoknadFormComponents';
@@ -31,56 +32,64 @@ const DinSituasjon = ({ onResetSoknad, onValidSubmit, soknadStepsConfig }: StepC
     const { harBruktOmsorgsdagerEtter1Juli } = values;
     const stepId = StepID.DIN_SITUASJON;
 
+    const { arbeiderINorge } = values;
+    const kanFortsette = arbeiderINorge === YesOrNo.YES;
+
     return (
         <SoknadFormStep
             id={stepId}
             soknadStepsConfig={soknadStepsConfig}
+            showSubmitButton={kanFortsette}
             onResetSoknad={onResetSoknad}
             onValidSubmit={onValidSubmit}
             onStepCleanup={cleanupDinSituasjonStep}>
             <CounsellorPanel>
                 <FormattedMessage id="step.din_situasjon.veileder.intro" />
             </CounsellorPanel>
-            <FormBlock>
-                <SoknadFormComponents.YesOrNoQuestion
-                    name={SoknadFormField.borINorge}
-                    legend={intlHelper(intl, 'step.din_situasjon.form.borINorge.spm')}
-                    validate={validateYesOrNoIsAnswered}
-                />
-            </FormBlock>
-            <FormBlock>
-                <SoknadFormComponents.YesOrNoQuestion
-                    name={SoknadFormField.arbeiderINorge}
-                    legend={intlHelper(intl, 'step.din_situasjon.form.arbeiderINorge.spm')}
-                    validate={validateYesOrNoIsAnswered}
-                />
-            </FormBlock>
-            <FormBlock>
-                <SoknadFormComponents.CheckboxPanelGroup
-                    legend={intlHelper(intl, 'step.din_situasjon.form.arbeidssituasjon.spm')}
-                    name={SoknadFormField.arbeidssituasjon}
-                    checkboxes={getArbeidssituasjonOptions(intl)}
-                    validate={validateRequiredList}
-                />
-            </FormBlock>
-            <FormBlock>
-                <SoknadFormComponents.YesOrNoQuestion
-                    name={SoknadFormField.harBruktOmsorgsdagerEtter1Juli}
-                    legend={intlHelper(intl, 'step.din_situasjon.form.harBruktOmsorgsdagerEtter1Juli.spm')}
-                    validate={validateYesOrNoIsAnswered}
-                />
-            </FormBlock>
-            {harBruktOmsorgsdagerEtter1Juli === YesOrNo.YES && (
-                <FormBlock>
-                    <SoknadFormComponents.Input
-                        name={SoknadFormField.antallDagerBruktEtter1Juli}
-                        label={intlHelper(intl, 'step.din_situasjon.form.antallDagerBruktEtter1Juli.spm')}
-                        validate={validateRequiredNumber({ min: 1 })}
-                        inputMode="numeric"
-                        style={{ maxWidth: '4rem' }}
-                        maxLength={2}
-                    />
-                </FormBlock>
+            <FormQuestion
+                name={SoknadFormField.arbeiderINorge}
+                legend={intlHelper(intl, 'step.din_situasjon.form.arbeiderINorge.spm')}
+                validate={validateYesOrNoIsAnswered}
+                showStop={arbeiderINorge === YesOrNo.NO}
+                stopMessage="For å overføre dager ..."
+            />
+            {arbeiderINorge === YesOrNo.YES && (
+                <>
+                    <FormBlock>
+                        <SoknadFormComponents.YesOrNoQuestion
+                            name={SoknadFormField.borINorge}
+                            legend={intlHelper(intl, 'step.din_situasjon.form.borINorge.spm')}
+                            validate={validateYesOrNoIsAnswered}
+                        />
+                    </FormBlock>
+                    <FormBlock>
+                        <SoknadFormComponents.CheckboxPanelGroup
+                            legend={intlHelper(intl, 'step.din_situasjon.form.arbeidssituasjon.spm')}
+                            name={SoknadFormField.arbeidssituasjon}
+                            checkboxes={getArbeidssituasjonOptions(intl)}
+                            validate={validateRequiredList}
+                        />
+                    </FormBlock>
+                    <FormBlock>
+                        <SoknadFormComponents.YesOrNoQuestion
+                            name={SoknadFormField.harBruktOmsorgsdagerEtter1Juli}
+                            legend={intlHelper(intl, 'step.din_situasjon.form.harBruktOmsorgsdagerEtter1Juli.spm')}
+                            validate={validateYesOrNoIsAnswered}
+                        />
+                    </FormBlock>
+                    {harBruktOmsorgsdagerEtter1Juli === YesOrNo.YES && (
+                        <FormBlock>
+                            <SoknadFormComponents.Input
+                                name={SoknadFormField.antallDagerBruktEtter1Juli}
+                                label={intlHelper(intl, 'step.din_situasjon.form.antallDagerBruktEtter1Juli.spm')}
+                                validate={validateRequiredNumber({ min: 1 })}
+                                inputMode="numeric"
+                                style={{ maxWidth: '4rem' }}
+                                maxLength={2}
+                            />
+                        </FormBlock>
+                    )}
+                </>
             )}
         </SoknadFormStep>
     );
