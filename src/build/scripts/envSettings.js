825fd638e050d544250f598f51ebe170a73d30b4
Frode Hansen <frode.hansen@bekk.no>
2020-11-17 15:07:38 +0100

    Change login url (fix)

--- src/build/scripts/envSettings.js
+++ src/build/scripts/envSettings.js
@@ -9,7 +9,6 @@ function createEnvSettingsFile(settingsFile) {
                 LOGIN_URL: '${process.env.LOGIN_URL}',
                 PUBLIC_PATH: '${process.env.PUBLIC_PATH}',
                 UTILGJENGELIG: '${process.env.UTILGJENGELIG}',
-                PERSISTENCE: '${process.env.PERSISTENCE}',
                 APP_VERSION: '${process.env.APP_VERSION}',
                 APPSTATUS_PROJECT_ID: '${process.env.APPSTATUS_PROJECT_ID}',
                 APPSTATUS_DATASET: '${process.env.APPSTATUS_DATASET}',
