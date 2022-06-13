1ff397a3cd683d0ffd723f6ac86322088a2878fa
Aleksei Poroshin <69150024+poroshinaleksei@users.noreply.github.com>
2022-06-13 11:54:19 +0200

    Upd node ver, pkg upd (#475)
    * Upd node ver, pkg upd
    
    * Slettet ubrukte pkg

--- src/build/webpack/webpack.config.global.js
+++ src/build/webpack/webpack.config.global.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
@@ -54,11 +53,6 @@ const webpackConfig = {
                     },
                 ],
             },
-            {
-                test: /\.svg$/,
-                loader: 'svg-sprite-loader',
-                options: {},
-            },
         ],
     },
     plugins: [
@@ -70,9 +64,6 @@ const webpackConfig = {
         new ESLintPlugin({
             extensions: ['ts', 'tsx'],
             failOnWarning: false,
-        }),
-        new SpriteLoaderPlugin({
-            plainSprite: true,
         }),
     ],
 };
